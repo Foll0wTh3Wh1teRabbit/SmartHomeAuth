ce56916ee049fcfa99681d8fe770141615354168
NikitaKosarevProwideLabs <51489085+Foll0wTh3Wh1teRabbit@users.noreply.github.com>
2023-02-28 15:46:13 +0700

    Initial commit

---
+++ src/main/java/ru/nsu/smarthomeauth/SmartHomeAuthApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.smarthomeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHomeAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nsu/smarthomeauth/SmartHomeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.smarthomeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
